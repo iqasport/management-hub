beb58a75118834d5ff7a10ef50a48dbd9411e303
Declan Ramsay <declandr@gmail.com>
2021-06-06 22:55:58 +0100

    Add seeding and docker-compose for local development with instructions

--- README.md
+++ README.md
@@ -14,37 +14,19 @@ Please note that this project is released with a Contributor Code of Conduct. By
   </a>
 </p>
 
+---
 
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+## QuickStart Local Development
 
-## Getting Started
-
-First, run the development server:
-
-```bash
-npm run dev
-# or
-yarn dev
-```
+1. Start [Docker Desktop](https://www.docker.com/get-started)
+2. Run `yarn db:setup` which will start the docker container with Postgres, run migrations and seed the database
+3. `yarn install`
+4. `yarn dev`
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
-
 You will need a `.env.local` file to set environment variables - use `.env.example` as a base to build off.
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
+To tear down the database run `yarn docker:down`
 
 ## Key Technology and Documentation
 
@@ -53,9 +35,3 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 - [Prisma](https://www.prisma.io/docs/getting-started)
 - [ChakraUI](https://chakra-ui.com/docs/getting-started)
 - [Emotion](https://emotion.sh/docs/introduction)
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
---
+++ prisma/seed.ts
@@ -0,0 +1,267 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.users.create({
+    data: {
+      first_name: 'Declan',
+      last_name: 'Ramsay',
+      id: 257,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+  });
+  await prisma.national_governing_bodies.createMany({
+    data: [
+      {
+        name: 'Argentina',
+        website: 'https://www.facebook.com/asociaciondequidditch.arg/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Australia',
+        website: 'http://www.quidditch.org.au/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Austria',
+        website: 'https://quidditch.at/en/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Belgium',
+        website: 'https://www.quidditchbelgium.be/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Brazil',
+        website: 'https://abrquadribol.wordpress.com/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Canada',
+        website: 'http://quidditchcanada.com/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Catalonia',
+        website: 'https://www.facebook.com/QuidditchCatalunya/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Chile',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Czech Republic',
+        website: 'http://famfrpal.cz/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Denmark',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'France',
+        website: 'http://www.quidditch.fr/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Germany',
+        website: 'http://www.deutscherquidditchbund.de/index.php/en/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Hungary',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Iceland',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'India',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Indonesia',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Ireland',
+        website: 'https://www.facebook.com/QuidditchIreland/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Israel',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Italy',
+        website: 'http://www.italiaquidditch.com/index.php/it/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Japan',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Mexico',
+        website: 'https://www.qmxsport.org/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Netherlands',
+        website: 'http://www.quidditchnederland.nl/nl/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'New Zealand',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Norway',
+        website: 'http://rumpeldunk.no/en/971-2/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Pakistan',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Peru',
+        website: 'https://www.facebook.com/QuidditchPeru/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Poland',
+        website: 'https://www.facebook.com/PolskaLigaQuidditcha',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Portugal',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Republic of Korea',
+        website: 'https://www.facebook.com/quidditchkorea/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Romania',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Serbia',
+        website: 'http://www.iqasport.com/membership/member-ngbs',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Slovakia',
+        website: 'https://www.facebook.com/slovakquidditch/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Slovenia',
+        website: 'https://www.facebook.com/quidditch.ljubljana/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Spain',
+        website: 'https://www.facebook.com/AsociacionQuidditchEspana/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Sweden',
+        website: 'https://www.facebook.com/QuidditchSverige/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Switzerland',
+        website: 'http://www.swissquidditch.ch/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Turkey',
+        website: 'http://quidditchdernegi.org/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Uganda',
+        website: 'https://www.facebook.com/Quidditch-Uganda-1451741015090784/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'United Kingdom',
+        website: 'https://www.quidditchuk.org/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'United States',
+        website: 'https://www.usquidditch.org/',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ],
+  });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
