f4044e74288a6dc6eb3f8245c1344e02b05f0208
Declan Ramsay <declandr@gmail.com>
2021-09-18 17:35:36 +0100

    Remove flipper tables

---
+++ prisma/migrations/20210918163525_remove_flipper_tables/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - You are about to drop the `flipper_features` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `flipper_gates` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "flipper_features";
+
+-- DropTable
+DROP TABLE "flipper_gates";
